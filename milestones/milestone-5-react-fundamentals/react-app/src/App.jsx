897f0a8136dad8d45b4e7ee186a325c890126809
lysanderuy <lysander.uy@gmail.com>
2026-02-24 11:15:42 +0800

    feat: add UseCallbackDemo component, fixes #22

--- milestones/milestone-5-react-fundamentals/react-app/src/App.jsx
+++ milestones/milestone-5-react-fundamentals/react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import Playbox from "./pages/SandBox";
+import Sandbox from "./pages/SandBox";
 
 export default function App() {
   return (
@@ -9,7 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/sandbox" element={<Playbox />} />
+        <Route path="/sandbox" element={<Sandbox />} />
       </Routes>
     </BrowserRouter>
   );
